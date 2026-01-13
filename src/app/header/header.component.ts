ab907762a84d8b56d46dcdaaec1f38a378cd5dba
Marcikadraga <marciprog@gmail.com>
2026-01-13 11:50:57 +0100

    add red mushroom for the header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { decodeToPixels, drawCanvas } from '../shared/utils/canvas';
+import { data } from '../shared/pictures/JsonPictures';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,17 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements AfterViewInit {
+  @ViewChild('logoCanvas', { static: true })
+  logoCanvas!: ElementRef<HTMLCanvasElement>;
+
+  ngAfterViewInit(): void {
+    // Replace this key with your logo key in the JSON
+    const logoFrame = data['red-mushroom'];
+
+    const pixels = decodeToPixels(logoFrame);
+
+    // Adjust size to taste
+    drawCanvas(this.logoCanvas.nativeElement, pixels, 4);
+  }
+}
